6f42674baff6b4037a717fdb299e9f1d11c803ac
Prajakt Gunjal <82635500+gunjal-prajakt26@users.noreply.github.com>
2021-12-30 16:13:51 +0000

    style colors added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const chalk =require("chalk");
 var readline = require('readline-sync');
 
-var user=readline.question("What is Your name? ");
-console.log("\n Welcome " +user+"\n");
-console.log("Welcome to * Prajakt's Quiz * \n");
+var user=readline.question(chalk.yellow("What is Your name? "));
+console.log(chalk.yellow("\n Welcome " +user+"\n"));
+console.log(chalk.yellow("Welcome to * Prajakt's Quiz * \n"));
 
 
 var score=0;
@@ -10,12 +11,14 @@ var score=0;
 function quiz(question, answer){
   var useranswer=readline.question(question);
   if(useranswer.toUpperCase() === answer.toUpperCase()){
-    console.log("Right!");
+    console.log(chalk.green("Right!"));
     score++;
   }else{
-    console.log("Wrong!");
+    console.log(chalk.red("Wrong!"));
   }
-console.log("Your score is "+ score+ "\n");
+console.log("--------------------------------------------\n");
+console.log(chalk.green("Your score is "+score+ "\n\n"));
+console.log("--------------------------------------------");
 }
 
 var questionList = [
@@ -27,13 +30,13 @@ var questionList = [
   que:"What is my birth month? \n a.March \n b.Feb \n c.May \n",
   ans:"a",
 },{
-  que:"In which company I work?\n a.TCS \n b.Capgemini \nc.Infosys\n ",
+  que:"In which company I work?\n a.TCS \n b.Capgemini \n c.Infosys\n ",
   ans:"b",
 },{
   que:"In which standerd I Wrote First Love Letter? \n a.first \n b.seventh \n c.Eleventh\n",
   ans:"a",
 },{
-  que:"What is my first crush's name? \n a. Mayuri \n b. sushmita \n c.pooja \n ",
+  que:"What is my first crush's name? \n a. Mayuri \n b. sushmita \n c. pooja \n ",
   ans:"c",
 },{
   que:"What is my hostel room number? \n a. G8 \n b.F6 \n c.F5 \n",
@@ -48,7 +51,7 @@ var questionList = [
   que:"My graduation passing year? \n a.2022 \n b.2019 \n c.2021\n",
   ans:"c",
 },{
-  que:"My favourite teacher?\n a.wahval\n b.Ambade \n c.Kanade ",
+  que:"My favourite teacher?\n a.wahval\n b.Ambade \n c.Kanade \n",
   ans:"a",
 },{
   que:"How many roomates I had in first year? \n a.2\n b.4 \n c.3 \n",
@@ -68,11 +71,11 @@ for(var i=0; i< questionList.length; i++){
 if(score === questionList.length){
 
   console.log("--------------------------------");
-  console.log("Congrats"+"\n"+"Best Scorers");
+  console.log(chalk.yellow.underline.bold("Congrats"+"\n"+"Best Scorers"));
   console.log("--------------------------------");
-  console.log("Prajakt");
-  console.log(user);
+  console.log(chalk.green("Prajakt"));
+  console.log(chalk.green(user));
 }else{
-  console.log("Your total Score is:"+ score+"\n");
-  console.log("Prajakt is best scorer");
+  console.log(chalk.yellow.underline("Your total Score is:"+ score+"\n"));
+  console.log(chalk.green("Prajakt is best scorer"));
 }
